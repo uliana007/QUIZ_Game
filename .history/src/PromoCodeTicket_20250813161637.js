771f28cb7ce84edf3b4e92f6eb347f1b69564e74
uliana007 <shuraevaul@mail.ru>
2025-08-13 16:48:52 +0300

    Добавлена +8 новых промиков

--- .history/src/PromoCodeTicket_20250813161637.js
+++ .history/src/PromoCodeTicket_20250813161637.js
@@ -26,12 +26,14 @@ export const promoList = [
   // 16 промиков
 
   { label: "Скидка до 25% на заказ по промокоду в FoodBand", code: "SportLiga", referralLink: "https://foodband.ru/pizza/?utm_source=yandex&utm_medium=cpc&utm_campaign=112904249&utm_content=16334861002&utm_term=food%20band&roistat_referrer=none&roistat_pos=premium_1&roistat=direct61_search_16334861002_food%20band&yclid=664196993353252863" },
-  { label: "Скидка 10% на заказ в Нияма", code: "BEF089021", referralLink: "https://niyama.ru/?utm_source=eLama-yandex&utm_medium=cpc&utm_campaign=Поиск%20%3A%20Доставка&utm_content=cid%7C101539010%7Cgid%7C5339487004%7Caid%7C15424509444%7Cadp%7Cno%7Cdvc%7Cdesktop%7Cpid%7C48583740956%7Crid%7C48583740956%7Cdid%7C48583740956%7Cpos%7Cpremium1%7Cadn%7Csearch%7Ccrid%7C0%7C&utm_term=---autotargeting&yclid=4431288914067587071" },
-  { label: "Пицца Мега пепперони 31см на тонком тесте в подарок при заказе от 1700р по промокоду", code: "CITYADS", referralLink: "https://ipizza.ru/msk/"},
-  { label: "Скидка 12% на заказ от 1500 по промокоду, действует в Суши Мастер", code: "SportLiga", referralLink: "https://moskva.sushi-master.ru/?yclid=5998612535750950911" },
-  { label: "Скидка 12% на заказ от 1500 по промокоду, действует в Галерея Суши", code: "SportLiga", referralLink: "https://tyumen.sushi-gallery.ru/" },
-  { label: "Скидка 12% на заказ от 1500 по промокоду, действует в Мир Суши", code: "SportLiga", referralLink: "https://tyumen.mirsushi.com/" },
-  { label: "Скидка 10% на заказ в Lacoste", code: "SportLiga", referralLink: "https://lacoste.ru/catalog/sales/?utm_source=yandex&utm_medium=cpc&utm_campaign=search_pure_brands&utm_term=---autotargeting&utm_content=pid%7C53835051871%7Crid%7C53835051871%7Ccid%7C76681676%7Cct%7Ctype1%7Cgid%7C5518396521%7Caid%7C16689731658%7Cap%7Cno%7Capt%7Cnone%7Cdt%7Cdesktop%7Catn%7C%7Catid%7C53835051871%7Cpos%7C2%7Cpost%7Cpremium%7Csrc%7Cnone%7Csrct%7Csearch%7Crgn%7CМосква%7Crgid%7C213&yclid=17026057983130337279"},
+
+  { label: "Скидка 12% на заказ от 3900 ₽ на первую покупку в GLOBUS", code: "a12sale", referralLink: "http://tplto.com/d3e4f605/" },
+  { label: "Скидка 5% на заказ в магазине Braun", code: "sale5", referralLink: "https://tplto.com/d6e4f605/" },
+  { label: "Скидка 1000 ₽ от суммы корзины 20 000 рублей в Бигам", code: "advcake1000r", referralLink: "https://tplto.com/d7e4f605/" },
+  { label: "Скидка 3% при покупке от 1 000 руб в Максидом", code: "advertise0507", referralLink: "https://tplto.com/dfe4f605/" },
+  { label: "Скидка 12% на заказ от 3900 ₽ на первую покупку в GLOBUS", code: "a12sale", referralLink: "http://tplto.com/d3e4f605/" },
+  { label: "Скидка 5% на заказ в магазине Braun", code: "sale5", referralLink: "https://tplto.com/d6e4f605/" },
+  { label: "Скидка 1000 ₽ от суммы корзины 20 000 рублей в Бигам", code: "advcake1000r", referralLink: "https://tplto.com/d7e4f605/" },
    // +8 промиков
 
   // внизу промокоды из синий таблицы, без реферальной ссылки (9 штук)
